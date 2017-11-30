98a1b75b6f09a31262c58bd06dafbde92ba4a5cb
Samuel Thomas <samvthom16@gmail.com>
2017-11-30 09:26:21 +0530

    [#22] - update plugin description

--- pf-rest.php
+++ pf-rest.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Publish on the Front End Using Rest API
 Plugin URI: http://wordpress.org/plugins/publishing-on-frontend/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Description: Publishing on front end using WP REST API. Using the WP Backbone API and REST API. 
 Author: Sputznik
 Version: 1.0
 Author URI: http://sputznik.com/
